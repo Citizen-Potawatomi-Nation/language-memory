9af04ddbb4e0db75163704e581dda30b0f7ce5a0
Nathan Hawkins <nathan.hawkins90@gmail.com>
2023-06-01 14:47:15 -0400

    new push

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import './App.css'
 import SingleCard from "./components/SingleCard";
 import LevelSelect from "./components/LevelSelect";
 
-const animalCards = [
-    { "src": "/img/animals/bzhew.jpg", matched: false },
-    { "src": "/img/animals/gagingezhi.jpg", matched: false },
-    { "src": "/img/animals/gedgene.jpg", matched: false },
-    { "src": "/img/animals/meingen.jpg", matched: false },
-    { "src": "/img/animals/moewe.jpg", matched: false },
-    { "src": "/img/animals/mozo.jpg", matched: false },
-    { "src": "/img/animals/msegek.jpg", matched: false },
-    { "src": "/img/animals/mzhewe.jpg", matched: false },
-    { "src": "/img/animals/nijan.jpg", matched: false },
-    { "src": "/img/animals/wakwshe.jpg", matched: false },
-    { "src": "/img/animals/yabe.jpg", matched: false },
-    { "src": "/img/animals/zhegak.jpg", matched: false }
-]
+const cardsList = {
+    animals: [
+        { "src": "/img/animals/bzhew.jpg", matched: false },
+        { "src": "/img/animals/gagingezhi.jpg", matched: false },
+        { "src": "/img/animals/gedgene.jpg", matched: false },
+        { "src": "/img/animals/meingen.jpg", matched: false },
+        { "src": "/img/animals/moewe.jpg", matched: false },
+        { "src": "/img/animals/mozo.jpg", matched: false },
+        { "src": "/img/animals/msegek.jpg", matched: false },
+        { "src": "/img/animals/mzhewe.jpg", matched: false },
+        { "src": "/img/animals/nijan.jpg", matched: false },
+        { "src": "/img/animals/wakwshe.jpg", matched: false },
+        { "src": "/img/animals/yabe.jpg", matched: false },
+        { "src": "/img/animals/zhegak.jpg", matched: false },
+        { "src": "/img/animals/gdede.jpg", matched: false },
+        { "src": "/img/animals/gnebek.jpg", matched: false },
+        { "src": "/img/animals/gnozhe.jpg", matched: false },
+        { "src": "/img/animals/kno.jpg", matched: false },
+        { "src": "/img/animals/mko.jpg", matched: false },
+        { "src": "/img/animals/mshike.jpg", matched: false },
+        { "src": "/img/animals/zheshko.jpg", matched: false }
+    ],
+    colors: [
+        { "src": "/img/colors/gizhgok-nade.jpg", matched: false },
+        { "src": "/img/colors/mkedewa.jpg", matched: false },
+        { "src": "/img/colors/mnwade.jpg", matched: false },
+        { "src": "/img/colors/mskwa.jpg", matched: false },
+        { "src": "/img/colors/skebgya.jpg", matched: false },
+        { "src": "/img/colors/wabshkya.jpg", matched: false },
+        { "src": "/img/colors/wjebgwa.jpg", matched: false },
+        { "src": "/img/colors/wzawa_brown.jpg", matched: false },
+        { "src": "/img/colors/wzawa_orange.jpg", matched: false },
+        { "src": "/img/colors/wzawa_yellow.jpg", matched: false }
+    ],
+    bodyparts: [
+        { "src": "/img/bodyparts/ndon.jpg", matched: false },
+        { "src": "/img/bodyparts/njash.jpg", matched: false },
+        { "src": "/img/bodyparts/njigwen.jpg", matched: false },
+        { "src": "/img/bodyparts/nkaden.jpg", matched: false },
+        { "src": "/img/bodyparts/nnech.jpg", matched: false },
+        { "src": "/img/bodyparts/nnek.jpg", matched: false },
+        { "src": "/img/bodyparts/nshkizhek.jpg", matched: false },
+        { "src": "/img/bodyparts/nshtegwan.jpg", matched: false },
+        { "src": "/img/bodyparts/ntog.jpg", matched: false },
+        { "src": "/img/bodyparts/nzet.jpg", matched: false }
+    ],
+    opposites: [
+        { "src": "/img/opposites/bkonya.jpg", "matchId": "1", matched: false },
+        { "src": "/img/opposites/gizhget.jpg", "matchId": "1", matched: false },
+        { "src": "/img/opposites/dbek-gizes.jpg", "matchId": "2", matched: false },
+        { "src": "/img/opposites/gizes.jpg", "matchId": "2", matched: false },
+        { "src": "/img/opposites/gzhate.jpg", "matchId": "3", matched: false },
+        { "src": "/img/opposites/ksenya.jpg", "matchId": "3", matched: false },
+        { "src": "/img/opposites/mno-bkonya.jpg", "matchId": "4", matched: false },
+        { "src": "/img/opposites/mno-waben.jpg", "matchId": "4", matched: false },
+        { "src": "/img/opposites/ndoki.jpg", "matchId": "5", matched: false },
+        { "src": "/img/opposites/nneba.jpg", "matchId": "5", matched: false }
+    ]
+}
 
-const colorCards = [
-    { "src": "/img/colors/black.jpg", matched: false },
-    { "src": "/img/colors/blue.jpg", matched: false },
-    { "src": "/img/colors/green.jpg", matched: false },
-    { "src": "/img/colors/pink.jpg", matched: false },
-    { "src": "/img/colors/purple.jpg", matched: false },
-    { "src": "/img/colors/red.jpg", matched: false },
-    { "src": "/img/colors/white.jpg", matched: false },
-    { "src": "/img/colors/yellow.jpg", matched: false }
-]
 function App() {
     const [cards, setCards] = useState([])
     const [turns, setTurns] = useState(0)
@@ -35,8 +70,8 @@ function App() {
     const [choiceTwo, setChoiceTwo] = useState(null)
     const [disabled, setDisabled] = useState(false)
     const [level, setLevel] = useState(null)
-    const [cardsPerRow, setCardsPerRow] = useState(6);
-    const [maxWidth, setMaxWidth] = useState('1100px');
+    const [cardsPerRow, setCardsPerRow] = useState(4);
+    const [maxWidth, setMaxWidth] = useState('800px');
 
     const endGame = () => {
         setLevel(null);
@@ -44,70 +79,122 @@ function App() {
         setMaxWidth('1100px');
     }
 
-    const shuffleCards = () => {
-        const cardImages = level === 'animals' ? animalCards : colorCards;
-        const shuffledCards = [...cardImages, ...cardImages]
-            .sort(() => Math.random() - 0.5)
-            .map((card) => ({...card, id: Math.random() }))
+    const shuffleCards = useCallback(() => {
+        const cardImages = cardsList[level];
+        const selectedCards = [];
+        const selectedMatchIds = [];
 
-        setChoiceOne(null)
-        setChoiceTwo(null)
-        setCards(shuffledCards)
-        setTurns(0)
-
-        const cardCount = shuffledCards.length;
-
-        let columns;
-        let width;
-        if (cardCount <= 4) {
-            columns = 2;
-            width = '275px';
-        } else if (cardCount <= 16) {
-            columns = 4;
-            width = '825px';
-        } else if (cardCount <= 24) {
-            columns = 6;
-            width = '1100px';
+        if (level === 'opposites') {
+            cardImages.forEach((card) => {
+                if (!selectedMatchIds.includes(card.matchId)) {
+                    selectedCards.push(card);
+                    const matchingCard = cardImages.find((c) => c.matchId === card.matchId && c !== card);
+                    if (matchingCard) {
+                        selectedCards.push(matchingCard);
+                        selectedMatchIds.push(card.matchId);
+                    }
+                }
+            });
         } else {
-            columns = Math.ceil(Math.sqrt(cardCount));
-            width = `${columns * 200}px`;
+            const availableCards = [...cardImages];
+            while (selectedCards.length < 16) {
+                const randomIndex = Math.floor(Math.random() * availableCards.length);
+                const randomCard = availableCards[randomIndex];
+                selectedCards.push(randomCard);
+                selectedCards.push({ ...randomCard, id: Math.random() });
+                availableCards.splice(randomIndex, 1);
+            }
         }
 
-        setCardsPerRow(columns);
-        setMaxWidth(width);
-    }
+        const shuffledCards = selectedCards.sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
+
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setCards(shuffledCards);
+        setTurns(0);
+
+        setCardsPerRow(4);
+        setMaxWidth('800px');
+    }, [level]);
+
+
+
 
     const handleChoice = (card) => {
         const flipSoundNumber = Math.floor(Math.random() * 6) + 1;
         const flipAudio = new Audio(`/flip${flipSoundNumber}.mp3`);
-        flipAudio.play();
+        flipAudio.volume = 0.5; // Adjust the volume here (0.5 means 50% volume)
 
-        choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-    }
+        const playFlipAudio = () => {
+            flipAudio.play();
+            choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+        };
+
+        if (choiceOne && choiceTwo) {
+            setDisabled(true);
+            let isMatch;
+
+            if (choiceOne.matchId && choiceTwo.matchId) {
+                isMatch = choiceOne.matchId === choiceTwo.matchId;
+            } else {
+                isMatch = choiceOne.src === choiceTwo.src;
+            }
+
+            if (isMatch) {
+                const audioSrc = (choiceOne.matchId ? choiceOne.matchId : choiceOne.src).replace('.jpg', '.mp3');
+                const audio = new Audio(audioSrc);
+                audio.volume = 0.2;
+                audio.addEventListener('ended', () => {
+                    setCards((prevCards) => {
+                        return prevCards.map((card) => {
+                            if (
+                                card.src === choiceOne.src ||
+                                (card.matchId && card.matchId === choiceOne.matchId)
+                            ) {
+                                return { ...card, matched: true };
+                            } else {
+                                return card;
+                            }
+                        });
+                    });
+                    setTimeout(() => resetTurn(), 1000);
+                });
+                audio.play();
+            } else {
+                setTimeout(() => resetTurn(), 1000);
+            }
+        } else {
+            playFlipAudio();
+        }
+    };
+
 
     useEffect(() => {
         if (choiceOne && choiceTwo) {
             setDisabled(true);
-            if (choiceOne.src === choiceTwo.src) {
-                const correctAudio = new Audio('/correct.mp3');
-                correctAudio.play();
+            let isMatch;
 
-                correctAudio.onended = () => {
-                    const audioSrc = choiceOne.src.replace('.jpg', '.mp3');
-                    const audio = new Audio(audioSrc);
-                    audio.play();
-                };
+            if (choiceOne.matchId && choiceTwo.matchId) {
+                isMatch = choiceOne.matchId === choiceTwo.matchId;
+            } else {
+                isMatch = choiceOne.src === choiceTwo.src;
+            }
+
+            if (isMatch) {
+                const audioSrc = (choiceOne.matchId ? choiceOne.matchId : choiceOne.src).replace('.jpg', '.mp3');
+                const audio = new Audio(audioSrc);
+                audio.play();
 
                 setCards(prevCards => {
                     return prevCards.map(card => {
-                        if (card.src === choiceOne.src) {
-                            return {...card, matched: true, src: card.src.replace('.jpg', '-w.jpg')}
+                        if (card.src === choiceOne.src || (card.matchId && card.matchId === choiceOne.matchId)) {
+                            return {...card, matched: true}
                         } else {
                             return card
                         }
                     })
                 });
-                resetTurn();
+                setTimeout(() => resetTurn(), 1000);
             } else {
                 setTimeout(() => resetTurn(), 1000);
             }
@@ -118,18 +205,18 @@ function App() {
         setChoiceOne(null)
         setChoiceTwo(null)
         setTurns(prevTurns => prevTurns + 1)
-        setDisabled(false)
+        setTimeout(() => setDisabled(false), 500);  // wait for 500 ms (or the time of your flip animation) before enabling the cards again
     }
 
     useEffect(() => {
         if (level) {
             shuffleCards();
         }
-    }, [level])
+    }, [level, shuffleCards]);
 
     return (
-        <div className="App" style={{ maxWidth }}>
-            <h1>language match game</h1>
+        <div className="App" style={{ maxWidth: level ? '800px' : '1140px' }}>
+            <h1>Bodéwadmimwen Match Game</h1>
             {!level ? <LevelSelect setLevel={setLevel}/> : (
                 <div>
                     <div className="tools">
--- src/components/LevelSelect.js
+++ src/components/LevelSelect.js
@@ -1,12 +1,60 @@
+import { useState } from 'react';
 import './LevelSelect.css'
 
 function LevelSelect({ setLevel }) {
+    const [flippedCard, setFlippedCard] = useState(null);
+    const [enlargedCard, setEnlargedCard] = useState(null);
+
+    const handleCardClick = (level) => {
+        setFlippedCard(level);
+        setTimeout(() => {
+            setEnlargedCard(level);
+        }, 500); // adjust as needed
+    };
+
+    const handleButtonClick = (level) => {
+        setLevel(level);
+        setFlippedCard(null);
+        setEnlargedCard(null);
+    }
+
+    const closeCard = (event) => {
+        event.stopPropagation();
+        setFlippedCard(null);
+        setEnlargedCard(null);
+    };
+
     return (
-        <div className="level-select">
+        <>
             <h2>Select a Level</h2>
-            <img src="/img/animals-card.jpg" alt="animals" onClick={() => setLevel('animals')} />
-            <img src="/img/colors-card.jpg" alt="colors" onClick={() => setLevel('colors')} />
-        </div>
+            <div className="level-select">
+                {['animals', 'colors', 'bodyparts', 'opposites'].map((level, index) => (
+                    <div key={index} className="level-card-wrapper">
+                        <div
+                            className={`level-card ${flippedCard === level ? 'flipped' : ''} ${enlargedCard === level ? 'enlarged' : ''}`}
+                            onClick={() => handleCardClick(level)}
+                        >
+                            <img src={`/img/${level}-card.jpg`} alt={level} className="front" />
+                            <div className="back">
+                                <img src="/img/blank-card.jpg" alt="blank" />
+                                {enlargedCard === level && (
+                                    <>
+                                        <button onClick={() => handleButtonClick(level)}>
+                                            Start Game
+                                        </button>
+                                        <span
+                                            className="close-btn"
+                                            alt="Close"
+                                            onClick={(e) => { e.stopPropagation(); setEnlargedCard(null); setFlippedCard(null); }}
+                                        >x</span>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
 
